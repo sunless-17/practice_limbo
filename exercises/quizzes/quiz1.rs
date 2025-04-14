a9e5f167caba70d30f16c4c9d37732414c968665
Sunless <sunless17@tutamail.com>
2025-04-14 19:56:12 +0300

    feat: learn if let and match

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,12 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(i: u8) -> u8 {
+    // use if let or match
+    if let i =  {
+        
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
